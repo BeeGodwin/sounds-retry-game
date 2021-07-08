d098aee71147fd1086d2eb97921ab40ab525aec7
Bee Godwin <bee.godwin@bbc.co.uk>
2021-07-08 10:08:38 +0100

    Flesh out the SoundManager class, which is now properly event driven. Plays random sounds for jumping and losing along with a single sfx for scoring.

--- Modules/RetryGame/Sources/RetryGame/AudioPlayer/SoundManager.swift
+++ Modules/RetryGame/Sources/RetryGame/AudioPlayer/SoundManager.swift
@@ -1,18 +1,84 @@
 import AVFoundation
+import GameplayKit
 
 class SoundManager: ObserverProtocol {
-    
-    var audioPlayer: AVAudioPlayer?
-    
+        
     var soundPlayers = [Sound: [AVAudioPlayer]]()
     
     init(eventBus: EventBusProtocol, soundDefinitions: [Sound: (String, String)]) {
-        eventBus.subscribe(to: .control, with: self)
+        
         let loader = AudioLoader()
         soundPlayers = loader.load(from: soundDefinitions)
+        
+        eventBus.subscribe(to: .control, with: self)
+        eventBus.subscribe(to: .score, with: self)
+        eventBus.subscribe(to: .game, with: self)
+        
     }
     
-    func receiveEvent(_ event: EventProtocol) {
-        // noop for now
+    func receiveEvent(_ message: EventProtocol) {
+        switch message.channel {
+        case .control:
+            if let controlEvent = message.event as? ControlEvent {
+                handleControlEvent(controlEvent)
+            }
+        case .game:
+            if let gameEvent = message.event as? GameEvent {
+                handleGameEvent(gameEvent)
+            }
+        case .score:
+            if let scoreEvent = message.event as? ScoreEvent {
+                handleScoreEvent(scoreEvent)
+            }
+        default:
+            print("unhandled event")
+        }
     }
+    
+    private func handleScoreEvent(_ event: ScoreEvent) {
+        switch event {
+        case .incrementBy:
+            playAudio(.score)
+        default:
+            noop()
+        }
+    }
+    
+    private func handleControlEvent(_ event: ControlEvent) {
+        switch event {
+        case .jump:
+            playAudio(.jump)
+        default:
+            noop()
+        }
+    }
+    
+    private func handleGameEvent(_ event: GameEvent) {
+        switch event {
+        case .gameOver:
+            fadeMusic()
+            playAudio(.die)
+        case .gameStart:
+            playMusic()
+        default:
+            noop()
+        }
+    }
+    
+    private func playAudio(_ sound: Sound) {
+        guard let players = soundPlayers[sound] else { return }
+        if players.isEmpty { return }
+        let random = GKRandomDistribution(forDieWithSideCount: players.count)
+        players[random.nextInt() - 1].play()
+    }
+    
+    private func playMusic() {
+        
+    }
+    
+    private func fadeMusic() {
+        
+    }
+    
+    private func noop () {}
 }
